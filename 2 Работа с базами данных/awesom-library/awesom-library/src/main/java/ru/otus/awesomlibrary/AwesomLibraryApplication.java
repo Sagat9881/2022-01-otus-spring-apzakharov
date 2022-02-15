180151b7eb98e55ed95cdccb3529c79643019d69
VTB4056605 <apzaharov@dev.vtb.ru>
2022-02-15 10:18:46 +0500

    initial commit

---
+++ 2 Работа с базами данных/awesom-library/awesom-library/src/main/java/ru/otus/awesomlibrary/AwesomLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.awesomlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomLibraryApplication.class, args);
+	}
+
+}
---
+++ 2 Работа с базами данных/awesom-library/awesom-library/src/test/java/ru/otus/awesomlibrary/AwesomLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.awesomlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
